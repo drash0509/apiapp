29167e92542d59f1d00d41dc3f800ee1c4a5547e
drash0509 <142102052+drash0509@users.noreply.github.com>
2024-06-17 07:29:57 +0530

    tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <text className='text-blue-600'>
+        hii
+      </text>
+    </div>
+  )
+}
+
+export default App
